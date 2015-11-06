ba479c0e02f8a480f97d789287fe3085ea1a4c3f
atomaja <atomaja@sollan.com>
2015-11-06 12:03:58 +0100

    Refactor of ThreeIpClassesRouter class

--- src/main/java/pl/tomaja/service/impl/ThreeIpClassesRouter.java
+++ src/main/java/pl/tomaja/service/impl/ThreeIpClassesRouter.java
@@ -1,5 +1,8 @@
 package pl.tomaja.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import com.google.common.base.Optional;
@@ -16,34 +19,31 @@ import pl.tomaja.service.RouterService;
  */
 public class ThreeIpClassesRouter implements RouterService {
 
-	private static final int SECOND_CLASS_RANGE = 500;
+	private static final int 
+		THIRD_CLASS_NUMBER = 3,
+		SECOND_CLASS_NUMBER = 2,
+		FIRST_CLASS_NUMBER = 1,
+		SECOND_CLASS_RANGE = 500,
+		FIRST_CLASS_RANGE = 100;
 
-	private static final int FIRST_CLASS_RANGE = 100;
-
-	@Inject
-	private DatabaseService class1Db;
-
-	@Inject
-	private DatabaseService class2Db;
-
-	@Inject
-	private DatabaseService class3Db;
-
+	private final Map<Integer, DatabaseService> databases = new HashMap<>();
+	
 	@Inject
 	private LogService logService;
 
 	@Inject
+	public ThreeIpClassesRouter(DatabaseService class1Db, DatabaseService class2Db, DatabaseService class3Db) {
+		databases.put(FIRST_CLASS_NUMBER, class1Db);
+		databases.put(SECOND_CLASS_NUMBER, class2Db);
+		databases.put(THIRD_CLASS_NUMBER, class3Db);
+	}
+	
+	@Inject
 	private void init(ExampleDataService exampleDataService) {
 		for (EndPoint endPoint : exampleDataService.getExampleData()) {
-			int ip = endPoint.getIp();
 			try {
-				if (ip < FIRST_CLASS_RANGE) {
-					class1Db.addEndPoint(endPoint);
-				} else if (ip < SECOND_CLASS_RANGE) {
-					class2Db.addEndPoint(endPoint);
-				} else {
-					class3Db.addEndPoint(endPoint);
-				}
+				DatabaseService service = getDatabase(determineClass(endPoint.getIp()));
+				service.addEndPoint(endPoint);
 			} catch (DatabaseException e) {
 				logService.log(LogLevel.ERROR, "Unable to add new endpoint: " + e.getMessage());
 			}
@@ -54,15 +54,11 @@ public class ThreeIpClassesRouter implements RouterService {
 
 	@Override
 	public void route(int ip, String message) {
-		if (ip < FIRST_CLASS_RANGE) {
-			findEndPointEndSend(ip, message, class1Db, 1);
-		} else if (ip < SECOND_CLASS_RANGE) {
-			findEndPointEndSend(ip, message, class2Db, 2);
-		} else {
-			findEndPointEndSend(ip, message, class3Db, 3);
-		}
+		int classNumber = determineClass(ip);
+		DatabaseService service = getDatabase(classNumber);
+		findEndPointEndSend(ip, message, service, classNumber);
 	}
-
+	
 	private void findEndPointEndSend(int ip, String message, DatabaseService db, int classId) {
 		Optional<EndPoint> endPoint = db.findEndPoint(ip);
 		if (!endPoint.isPresent()) {
@@ -72,5 +68,19 @@ public class ThreeIpClassesRouter implements RouterService {
 		final EndPoint endPointValue = endPoint.get();
 		endPointValue.receive(message);
 		logService.log(LogLevel.INFO, String.format("Endpoint %s received the message: %s", endPointValue.getName(), message));
+	}
+	
+	private int determineClass(int ip) {
+		if(ip < FIRST_CLASS_RANGE) {
+			return FIRST_CLASS_NUMBER;
+		} else if(ip < SECOND_CLASS_RANGE) {
+			return SECOND_CLASS_NUMBER;
+		} else {
+			return THIRD_CLASS_NUMBER;
+		}
+	}
+	
+	private DatabaseService getDatabase(int classNumber) {
+		return databases.get(classNumber);
 	}
 }
